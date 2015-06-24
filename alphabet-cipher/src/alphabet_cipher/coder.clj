393c5d4195ea00ca0ac237d9f46a7a5d626c4f9e
smith750 <jksmith@kuali.co>
2015-06-24 18:48:43 -0400

    complete the alphabet cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,45 @@
 (ns alphabet-cipher.coder)
 
+(defn ascii-offset [c] (- (int c) 97))
+
+(defn translate [keyword message operation]
+  (let
+    [keyword-salt (take (count message) (cycle keyword))]
+    (apply str (map #(apply operation %) (map vector keyword-salt message)))
+    )
+  )
+
+(defn encode-char [row col]
+  (let
+    [row-offset (ascii-offset row)
+     col-offset (ascii-offset col)
+     wrap-around (fn [c]
+                   (if (> c 122)
+                     (char (- c 26))
+                     (char c))
+                   )
+     ]
+    (char (wrap-around (+ row-offset col-offset 97)))
+    )
+  )
+
 (defn encode [keyword message]
-  "encodeme")
+  (translate keyword message encode-char)
+  )
+
+(defn decode-char [row code]
+  (let
+    [row-offset (ascii-offset row)
+     wrap-around (fn [c]
+                   (if (< c 97)
+                     (char (+ c 26))
+                     (char c))
+                   )
+     ]
+    (char (wrap-around (- (int code) row-offset)))
+    )
+  )
 
 (defn decode [keyword message]
-  "decodeme")
+  (translate keyword message decode-char)
+  )
